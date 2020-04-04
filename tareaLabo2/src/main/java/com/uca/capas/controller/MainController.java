25e850d0bd9f17a1e48224d1895fd9ee494bf9a4
MarioDeLeonRecinos <37156087+mariocdeleon@users.noreply.github.com>
2020-04-04 16:15:42 -0600

    commit inicial y final

---
+++ tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.uca.capas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.uca.capas.domain.Usuario;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/login")
+	public String enviarForm(Usuario usuario) {
+		return "Login";
+	}
+	
+	@PostMapping("/validacion")
+	public String validacion(Usuario usuario) {
+		//Credenciales Usuario: Ceci password: admin
+		if(usuario.getNombre().equals("Ceci") && usuario.getPassword().equals("admin")) return "MostrarMensajeV";
+		else return "MostrarMensajeF";
+	}
+	
+}
